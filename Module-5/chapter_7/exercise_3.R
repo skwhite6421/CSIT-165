dd7a565f5216c875223e803eda84f1de33b180f4
Troy Alva <avlayort@gmail.com>
2020-02-17 16:03:40 -0800

    Complete module 5 exercises

---
+++ Module-5/chapter_7/exercise_3.R
@@ -0,0 +1,25 @@
+# Exercise 3: vector practice
+
+# Create a vector `words` of 6 (or more) words.
+# You can Google for a "random word generator" if you wish!
+
+
+# Create a vector `words_of_the_day` that is your `words` vector with the string
+# "is the word of the day!" pasted on to the end using `paste0()`.
+# BONUS: Surround the word in quotes (e.g., `'data' is the word of the day!`)
+# Note that the results are more obviously correct with single quotes.
+
+
+# Create a vector `a_f_words` which are the elements in `words` that start with 
+# "a" through "f"
+# Hint: use a comparison operator to see if the word comes before "f" alphabetically!
+# Tip: make sure all the words are lower-case, and only consider the first letter
+# of the word! `substring()` is a great function for isolating the first letter of 
+# the word alphabetically. This could be used inside the vector index with a compparison 
+# operator. Remember
+
+
+# Create a vector `g_m_words` which are the elements in `words` that start with 
+# "g" through "m"
+# Hint: You can do this in one statement or two statments, but if you use one 
+# statement you will need a `&` operator. 
