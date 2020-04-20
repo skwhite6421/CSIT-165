1be08db649a76736e2edfee40d93a97369314550
Troy Alva <avlayort@gmail.com>
2020-04-19 22:23:44 -0700

    Module 12 exercises

--- Vignettes/demonstration.R
+++ Vignettes/demonstration.R
@@ -1,7 +1,11 @@
+library(ggplot2)
+
+starwars <- as.data.frame(dplyr::starwars)
+
 ### Single variable plots
 #   Histograms
 ggplot(starwars) + 
-  geom_histogram(aes(x = birth_year), 
+  geom_histogram(mapping = aes(x = birth_year), 
                  na.rm = TRUE, binwidth = 10, fill = blue, color = lightblue3) + 
   labs(title = "Histogram of age distributions for starwars characters", 
        x = "Age (years)",
@@ -9,14 +13,18 @@ ggplot(starwars) +
 
 #   Box plots
 ggplot(starwars[starwars$gender != "none" & !is.na(starwars$gender),]) +
-  geom_boxplot(aes(x = gender, y = height), color = blue) + 
+  geom_boxplot(aes(x = gender, y = height), 
+               color = blue,
+               na.rm = TRUE) + 
   labs(title = "Boxplot of height distributions for different gendered starwars characters",
        x = "Gender", 
        y = "Height")
 
 #   Violin plots
 ggplot(starwars[starwars$gender != "none" & !is.na(starwars$gender),]) +
-  geom_violin(aes(x = gender, y = height), fill = blue) + 
+  geom_violin(aes(x = gender, y = height), 
+              fill = blue, 
+              na.rm = TRUE) + 
   labs(title = "Violin plot of height distributions for different gendered starwars characters",
        x = "Gender", 
        y = "Height")
@@ -26,12 +34,27 @@ ggplot(starwars[starwars$gender != "none" & !is.na(starwars$gender),]) +
 # Pie chart
 ggplot(starwars) +
   geom_bar(aes(x = "", y = gender, color = gender, fill = gender), 
-           na.rm = TRUE, stat = "identity", width = 1) +
+           na.rm = TRUE, 
+           stat = "identity", 
+           width = 1) +
   coord_polar("y", start=0) + 
-  labs(title = "Pie chart of different starwars genders")
+  labs(title = "Pie chart of different starwars genders",
+       x = "", 
+       y = "Gender")
 
+
+## Scatter plot
 ggplot(starwars) +
-  geom_bar(aes(x = "", y = eye_color, color = eye_color, fill = eye_color), 
-           na.rm = TRUE, stat = "identity", width = 1) +
-  coord_polar("y", start=0) + 
-  labs(title = "Pie chart of different starwars eye colors")
+  geom_point(mapping = aes(x = height, y = mass), 
+             na.rm = TRUE) + 
+  labs(title = "Scatter plot of height vs mass", 
+       x = "Height", 
+       y = "Mass")
+
+## Filtered scatter plot
+ggplot(starwars[-which.max(starwars$mass),]) +
+  geom_point(mapping = aes(x = height, y = mass), 
+             na.rm = TRUE) +
+  labs(title = "Scatter plot of height vs mass", 
+       x = "Height", 
+       y = "Mass")
